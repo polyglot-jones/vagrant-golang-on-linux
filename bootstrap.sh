db1405db57e9beb842f394f8a0f0eea9b4064850
Craig Jones <craigjones@chiefsimplicityofficer.com>
2015-01-05 18:16:38 -0800

    Fixed all the places that require referring to $HOME instead of ~ (tilde).

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,16 @@
 # This script is idempotent. There is no harm is running it multiple times.
 
 echo "==========  Starting Bootstrap.sh  ==========" &>> /vagrant/bootstrap.log
-
-ls -l /vagrant
+echo "HOME = $HOME" &>> /vagrant/bootstrap.log
 
 if [ -f "/vagrant/bash_aliases" ]; then
 	echo "bash_aliases exists" &>> /vagrant/bootstrap.log
-	if ! [ -f "~/.bash_aliases" ]; then
+	if ! [ -f "$HOME/.bash_aliases" ]; then
 		echo "Copying bash_aliases as ~/.bash_aliases" &>> /vagrant/bootstrap.log
-		cp /vagrant/bash_aliases ~/.bash_aliases 2>> /vagrant/bootstrap.log
-		chmod 644 ~/.bash_aliases
+		sudo cp /vagrant/bash_aliases $HOME/.bash_aliases 2>> /vagrant/bootstrap.log
+		sudo chmod 644 $HOME/.bash_aliases
 	fi
-	if [ -f "~/.bash_aliases" ]; then
+	if [ -f "$HOME/.bash_aliases" ]; then
 		source ~/.bash_aliases
 	else
 		echo "ERROR: FAILED TO COPY bash_aliases as ~/.bash_aliases" &>> /vagrant/bootstrap.log
@@ -22,8 +21,7 @@ fi
 
 if [ -f "/vagrant/local_config.sh" ]; then
 	echo "local_config.sh exists" &>> /vagrant/bootstrap.log
-	# chmod 770 "/vagrant/local_config.sh"
-    source /vagrant/local_config.sh
+	source /vagrant/local_config.sh
 fi
 
 sudo apt-get update 2>> /vagrant/bootstrap.log
@@ -40,10 +38,10 @@ git config --global user.name "$VC_NAME"
 git config --global user.email $VC_EMAIL
 bzr whoami "$VC_NAME <$VC_EMAIL>"
 # TODO Create a file called ~/.hgrc with Hg settings, including username=John Doe <johndoe@example.com> (under the [ui] section)
-if ! [ -f "~/.netrc" ]; then
+if ! [ -f "$HOME/.netrc" ]; then
 	echo "~/.netrc does not already exist. Creating it now." &>> /vagrant/bootstrap.log
-	echo "machine github.com login $GITHUB_APP_TOKEN" > ~/.netrc
-	if ! [ -f "~/.netrc" ]; then
+	sudo echo "machine github.com login $GITHUB_APP_TOKEN" > $HOME/.netrc
+	if ! [ -f "$HOME/.netrc" ]; then
 		echo "ERROR: FAILED TO CREATE ~/.netrc" &>> /vagrant/bootstrap.log
 	fi
 fi
