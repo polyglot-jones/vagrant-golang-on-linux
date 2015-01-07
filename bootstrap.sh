b7ca0e6b50763c88a4ff22c4388958347108fd4a
Craig Jones <craigjones@chiefsimplicityofficer.com>
2015-01-06 21:05:32 -0800

    The biggest change here is that a setting in local_config.sh now determines which version of Go is installed (e.g. 1.3 vs 1.4).

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
-# IMPORTANT: When this script is invoked as part of VAGRANT UP (i.e. as specified 
-# via  Vagrantfile), it executes while being logged in as root (and $HOME is /root). 
-# However, the VAGRANT SSH command logs you in as "vagrant" and $HOME is /home/vagrant.
-
 # This script is idempotent. There is no harm is running it multiple times.
 
+# Note: When this script is invoked as part of VAGRANT UP (i.e. as specified 
+# via  Vagrantfile), it executes while being logged in as root (and $HOME is /root). 
+# However, the VAGRANT SSH command logs you in as "vagrant" and $HOME is /home/vagrant.
+# This script therefore avoids using ~ and $HOME altogether (except as a temp folder for wget).
+
+
+######################################################   Load local_config.sh Settings
+if [ -f "/vagrant/local_config.sh" ]; then
+	echo "Loading local_config.sh" &>> /vagrant/bootstrap.log
+	source /vagrant/local_config.sh
+fi
+if [ -z "$GOVERSION" ]; then
+    GOVERSION="1.4"
+fi 
+
 echo "==========  Starting Bootstrap.sh  ==========" &>> /vagrant/bootstrap.log
+echo "GOVERSION = $GOVERSION" &>> /vagrant/bootstrap.log
+echo "VC_NAME = $VC_NAME" &>> /vagrant/bootstrap.log
+echo "VC_EMAIL = $VC_EMAIL" &>> /vagrant/bootstrap.log
+echo "GITHUB_APP_TOKEN = $GITHUB_APP_TOKEN" &>> /vagrant/bootstrap.log
+echo "---------------------------------------------" &>> /vagrant/bootstrap.log
+
 
 ######################################################   Linux Software Installs
-
 echo "Pointing apt-get to the PostgreSQL repository." &>> /vagrant/bootstrap.log
 echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
@@ -31,9 +47,9 @@ sudo -u postgres createdb --owner=vagrant vagrant 2>> /vagrant/bootstrap.log
 
 cd ~
 if [ ! -d "/usr/local/go" ]; then
-	echo "Installing the Go language compiler version 1.4" &>> /vagrant/bootstrap.log
-	wget -q https://storage.googleapis.com/golang/go1.4.linux-386.tar.gz 2>> /vagrant/bootstrap.log
-	tar -C /usr/local -xzf go1.4.linux-386.tar.gz 2>> /vagrant/bootstrap.log
+	echo "Installing the Go language compiler version $GOVERSION" &>> /vagrant/bootstrap.log
+	wget -q https://storage.googleapis.com/golang/go$GOVERSION.linux-386.tar.gz 2>> /vagrant/bootstrap.log
+	tar -C /usr/local -xzf go$GOVERSION.linux-386.tar.gz 2>> /vagrant/bootstrap.log
 	chown -R vagrant /usr/local/go
 fi
 
@@ -43,7 +59,6 @@ echo "Ensuring that /home/vagrant exists" &>> /vagrant/bootstrap.log
 mkdir -p /home/vagrant
 
 if [ -f "/vagrant/bash_aliases" ]; then
-	echo "bash_aliases exists" &>> /vagrant/bootstrap.log
 	if ! [ -f "/home/vagrant/.bash_aliases" ]; then
 		echo "Copying bash_aliases as /home/vagrant/.bash_aliases" &>> /vagrant/bootstrap.log
 		sudo cp /vagrant/bash_aliases /home/vagrant/.bash_aliases 2>> /vagrant/bootstrap.log
@@ -56,18 +71,8 @@ if [ -f "/vagrant/bash_aliases" ]; then
 	fi
 fi
 
-if [ -f "/vagrant/local_config.sh" ]; then
-	echo "Loading local_config.sh" &>> /vagrant/bootstrap.log
-	source /vagrant/local_config.sh
-fi
 
 ######################################################   Version Control Configuration
-
-
-echo "VC_NAME = $VC_NAME" &>> /vagrant/bootstrap.log
-echo "VC_EMAIL = $VC_EMAIL" &>> /vagrant/bootstrap.log
-echo "GITHUB_APP_TOKEN = $GITHUB_APP_TOKEN" &>> /vagrant/bootstrap.log
-
 git config --global user.name "$VC_NAME"
 git config --global user.email $VC_EMAIL
 bzr whoami "$VC_NAME <$VC_EMAIL>"
