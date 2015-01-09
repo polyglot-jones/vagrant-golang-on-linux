fab33a99b6a33e20864728fa71ac0582999a6cd3
Craig Jones <craigjones@chiefsimplicityofficer.com>
2015-01-08 17:32:47 -0800

    Added an option to install RabbitMQ; Changed the PostreSQL install to be optional; Changed the forwarded ports to 9000, 9001, and 9002.

--- Vagrantfile
+++ Vagrantfile
@@ -16,7 +16,9 @@ Vagrant.configure(2) do |config|
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
-  config.vm.network "forwarded_port", guest: 80, host: 9080
+  config.vm.network "forwarded_port", host: 9000, guest: 9000, auto_correct: true
+  config.vm.network "forwarded_port", host: 9001, guest: 9001, auto_correct: true
+  config.vm.network "forwarded_port", host: 9002, guest: 9002, auto_correct: true
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 
 # This script is idempotent. There is no harm is running it multiple times.
 
-# Note: When this script is invoked as part of VAGRANT UP (i.e. as specified 
-# via  Vagrantfile), it executes while being logged in as root (and $HOME is /root). 
+# Note: When this script is invoked as part of VAGRANT UP (i.e. as specified
+# via  Vagrantfile), it executes while being logged in as root (and $HOME is /root).
 # However, the VAGRANT SSH command logs you in as "vagrant" and $HOME is /home/vagrant.
 # This script therefore avoids using ~ and $HOME altogether (except as a temp folder for wget).
 
@@ -13,15 +13,17 @@ if [ -f "/vagrant/local_config.sh" ]; then
 	echo "Loading local_config.sh" &>> /vagrant/bootstrap.log
 	source /vagrant/local_config.sh
 fi
-if [ -z "$GOVERSION" ]; then
-    GOVERSION="1.4"
-fi 
+if [ -z "$GO_VERSION" ]; then
+    GO_VERSION="1.4"
+fi
 
 echo "==========  Starting Bootstrap.sh  ==========" &>> /vagrant/bootstrap.log
-echo "GOVERSION = $GOVERSION" &>> /vagrant/bootstrap.log
+echo "GO_VERSION = $GO_VERSION" &>> /vagrant/bootstrap.log
 echo "VC_NAME = $VC_NAME" &>> /vagrant/bootstrap.log
 echo "VC_EMAIL = $VC_EMAIL" &>> /vagrant/bootstrap.log
 echo "GITHUB_APP_TOKEN = $GITHUB_APP_TOKEN" &>> /vagrant/bootstrap.log
+echo "USE_POSTGRES = $USE_POSTGRES" &>> /vagrant/bootstrap.log
+echo "USE_RABBIT = $USE_RABBIT" &>> /vagrant/bootstrap.log
 echo "---------------------------------------------" &>> /vagrant/bootstrap.log
 
 
@@ -30,26 +32,39 @@ echo "Pointing apt-get to the PostgreSQL repository." &>> /vagrant/bootstrap.log
 echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
+echo "Pointing apt-get to the RabbitMQ repository." &>> /vagrant/bootstrap.log
+echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -
+
+
 echo "Updating apt-get" &>> /vagrant/bootstrap.log
 sudo apt-get update 2>> /vagrant/bootstrap.log
+sudo apt-get -y upgrade
 
 echo "Installing the 3 version-control systems" &>> /vagrant/bootstrap.log
 sudo apt-get install -y git mercurial bzr 2>> /vagrant/bootstrap.log
 
-echo "Installing PostgreSQL 9.3.x" &>> /vagrant/bootstrap.log
-sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3
-echo "Changing Postgre to trust mode (/etc/postgresql/9.3/main/pg_hba.conf)" &>> /vagrant/bootstrap.log
-sed -i "s/local\s*all\s*all\s*peer/local all all trust/" /etc/postgresql/9.3/main/pg_hba.conf 2>> /vagrant/bootstrap.log
-sudo /etc/init.d/postgresql restart 2>> /vagrant/bootstrap.log
-echo "Creating PostgreSQL user 'vagrant' as a superuser and an empty 'vagrant' database." &>> /vagrant/bootstrap.log
-sudo -u postgres createuser --superuser vagrant 2>> /vagrant/bootstrap.log
-sudo -u postgres createdb --owner=vagrant vagrant 2>> /vagrant/bootstrap.log
+if [ $USE_POSTGRES -eq "INSTALL" ]; then
+	echo "Installing PostgreSQL 9.3.x" &>> /vagrant/bootstrap.log
+	sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3
+	echo "Changing Postgre to trust mode (/etc/postgresql/9.3/main/pg_hba.conf)" &>> /vagrant/bootstrap.log
+	sed -i "s/local\s*all\s*all\s*peer/local all all trust/" /etc/postgresql/9.3/main/pg_hba.conf 2>> /vagrant/bootstrap.log
+	sudo /etc/init.d/postgresql restart 2>> /vagrant/bootstrap.log
+	echo "Creating PostgreSQL user 'vagrant' as a superuser and an empty 'vagrant' database." &>> /vagrant/bootstrap.log
+	sudo -u postgres createuser --superuser vagrant 2>> /vagrant/bootstrap.log
+	sudo -u postgres createdb --owner=vagrant vagrant 2>> /vagrant/bootstrap.log
+fi
+
+if [ $USE_RABBIT -eq "INSTALL" ]; then
+	echo "Installing RabbitMQ Server" &>> /vagrant/bootstrap.log
+	sudo apt-get install -y rabbitmq-server
+fi
 
 cd ~
 if [ ! -d "/usr/local/go" ]; then
-	echo "Installing the Go language compiler version $GOVERSION" &>> /vagrant/bootstrap.log
-	wget -q https://storage.googleapis.com/golang/go$GOVERSION.linux-386.tar.gz 2>> /vagrant/bootstrap.log
-	tar -C /usr/local -xzf go$GOVERSION.linux-386.tar.gz 2>> /vagrant/bootstrap.log
+	echo "Installing the Go language compiler version $GO_VERSION" &>> /vagrant/bootstrap.log
+	wget -q https://storage.googleapis.com/golang/go$GO_VERSION.linux-386.tar.gz 2>> /vagrant/bootstrap.log
+	tar -C /usr/local -xzf go$GO_VERSION.linux-386.tar.gz 2>> /vagrant/bootstrap.log
 	chown -R vagrant /usr/local/go
 fi
 
